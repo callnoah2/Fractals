6133f3f0f792b5a03fc65ad69ee6f7ce5a25151f
callnoah2 <A02361280@aggies.usu.edu>
2023-03-29 11:38:31 -0600

    Now program will wait for user to close window.

--- src/ImagePainter.py
+++ src/ImagePainter.py
@@ -1,10 +1,10 @@
-from tkinter import Tk, Canvas, PhotoImage
+from tkinter import Tk, Canvas, PhotoImage, mainloop
 import time
 import Mandelbrot
 import Phoenix
 import sys
 import Palette
-start = time.time()
+
 maxIter = 115
 
 # def getMin():
@@ -39,6 +39,7 @@ maxIter = 115
 def make_picture_of_fractal(fractal, canvas_size, whichFractal):
     """Paint a Fractal image into the TKinter PhotoImage canvas.
     Assumes the image is 640x640 pixels."""
+
     root = Tk()
     photo_image = PhotoImage(master=root, width=canvas_size, height=canvas_size)
     min_coord = (fractal['centerX'] - (fractal['axisLen'] / 2.0), fractal['centerY'] - (fractal['axisLen'] / 2.0))
@@ -50,9 +51,8 @@ def make_picture_of_fractal(fractal, canvas_size, whichFractal):
     canvas.create_image((canvas_size / 2, canvas_size / 2), image=photo_image, state="normal")
     canvas.pack()
 
-    area_in_pixels = 640 * 640
     size = abs(max_coord[0] - min_coord[0]) / canvas_size
-
+    start = time.time()
     row = canvas_size
     while row in range(canvas_size, 0, -1):
         cs = []
@@ -60,7 +60,7 @@ def make_picture_of_fractal(fractal, canvas_size, whichFractal):
             X = min_coord[0] + col * size
             Y = min_coord[1] + row * size
             if whichFractal == 1:
-                cp = Phoenix.phoenixIter(complex(X, Y),maxIter=102)
+                cp = Phoenix.phoenixIter(complex(X, Y), maxIter=102)
             else:
                 cp = Mandelbrot.mBrotIter(complex(X,Y), maxIter)
             color = Palette.getColor(cp)
@@ -71,3 +71,9 @@ def make_picture_of_fractal(fractal, canvas_size, whichFractal):
         fraction_of_pixels_written_so_far = (canvas_size - row) / canvas_size
         print(f"[{fraction_of_pixels_written_so_far:>4.0%}" + f"{'=' * int(34 * fraction_of_pixels_written_so_far):<33}]", end="\r", file=sys.stderr)
         row -= 1
+    # Save Image
+    file_name = f"{whichFractal}_fractal.png"  # choose a file name
+    photo_image.write(file_name, format="png")
+    print(f"\nDone in {time.time() - start:.3f} seconds!", file=sys.stderr)
+    print("Close the image window to exit the program", file=sys.stderr)
+    mainloop()
